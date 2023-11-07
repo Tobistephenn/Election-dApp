dbc53ff32049361ef4726ef6fe39f88d51ae36fd
Tobi Stephen <77818984+Tobistephenn@users.noreply.github.com>
2023-11-07 20:40:53 +0100

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-storage-layout'
+import 'hardhat-contract-sizer'
+import 'hardhat-deploy'
+import {config} from 'dotenv'
+
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    config()
+}
+
+export default {
+    contractSizer: {
+        strict: true
+    },
+    networks: {
+        hardhat: {
+            chainId: 33133,
+            allowUnlimitedContractSize: false,
+            loggingEnabled: false
+        },
+        local: {
+            url: 'http://localhost:8545',
+            chainId: 33133,
+            allowUnlimitedContractSize: false,
+            loggingEnabled: true
+        },
+        polygonMumbai: {
+            url: 'https://rpc.ankr.com/polygon_mumbai',
+            chainId: 80001,
+            loggingEnabled: true,
+            accounts: [process.env.PRIVATE_KEY],
+            saveDeployments: true,
+            zksync: false
+        }
+    },
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.20',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            }
+        ]
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        }
+    },
+    etherscan: {
+        apiKey: {
+            polygonMumbai: process.env.POLYGONSCAN_KEY
+        }
+    }
+}
